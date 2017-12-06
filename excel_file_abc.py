0796a77a752a1f530d68304d5d54e61c4efcbd15
Leo Gasteiger <lgasteiger@gmail.com>
2017-12-06 10:35:43 -0800

    excel_file_abc.py abstract base class
    excel_file_abc.py abstract base class shall provide the contact to create excel file processors using Python

---
+++ excel_file_abc.py
@@ -0,0 +1,12 @@
+'''
+ ##############################################################################################
+ # excel-file-abc.py - this abstract base class shall provide the contract on how to implement
+ #                     Python solutions for processing Microsoft Excel files. It shall use the 
+ #                     openpyxl library.
+ # 
+ # Date: 2017-12-06
+ #
+ # Author: Leo Gasteiger
+ ##############################################################################################
+'''
+
