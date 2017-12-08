caf5386ca2e7cc6c1ccce8267c1e5b8b4ef44be5
Leo Gasteiger <lgasteiger@gmail.com>
2017-12-07 16:02:56 -0800

    excel_file_abc.py init method
    excel_file_abc.py init method, getters, and setters implementation

--- excel_file_abc.py
+++ excel_file_abc.py
@@ -4,7 +4,7 @@
  #                     Python solutions for processing Microsoft Excel files. It shall use the 
  #                     openpyxl library.
  # 
- # Date: 2017-12-06
+ # Date: 2017-12-07
  #
  # Author: Leo Gasteiger
  ##############################################################################################
@@ -16,8 +16,27 @@ class Excel_File_ABC(ABC):
 
   __counter = 0
   
+  @abstractmethod
+  def __init__(self, hebprd_dir_path, fcfsprd_dir_path, cms_scripts_rept_dir_path):
+    tyep(self).__counter += 1
+    self.__hebprd_dir_path = hebprd_dir_path
+    self.__fcfsprd_dir_pat = fcfsprd_dir_path
+    self.__cms_scripts_rept_dir_path = cms_scripts_rept_dir_path
+  #end __init__
+  
   @classmethod
   def Excel_File_Instances(cls):
     return cls.__counter
   #end Excel_File_Instances
+  
+  @property
+  def hebprd_dir_path(self):
+    return self.__hebprd_dir_path    
+  #end hebprd_dir_path getter
+  
+  
+  @hebprd_dir_path.setter
+  def hebprd_dir_path(self, new_hebprd_dir_path):
+    self.__hebprd_dir_path = new_hebprd_dir_path
+  #end hebprd_dir_path setter
 #end Excel_File_ABC class
